fd220bedde55db30329fcb620982a8774e909cbc
Kyler Kramer <kyler@Kyler.Kylertk21>
2024-06-18 11:49:16 -0600

    flask server functional

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,32 +1,12 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+import os
 
-app = Flask("Three Little Birds Web App")
-app.secret_key= 'secret'
-app.config['USER_SIGN_UP'] = 'User Sign Up'
-app.config['USER_SIGN_IN'] = 'User Sign In'
+print("Work DIR: ", os.getcwd())
 
-#init Flask-SQLAlchemy
-db = SQLAlchemy()
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-db.init_app(app)
+template_dir = os.path.join(os.getcwd(), 'templates')
+print("Template folder: ", template_dir)
 
-#init Flask-Login
-login_manager = LoginManager(app)
+app = Flask("Three Little Birds", template_folder=template_dir)
+app.secret_key = os.environ['SECRET_KEY']
 
-#create database tables
-with app.app_context():
-    db.create_all()
-
-#login
-from app.models import User
-
-@login_manager.user_loader
-def load_user(id):
-    try:
-        return db.session.query(User).filter(User.id == id).one()
-    except:
-        return "User Not Found"
-    
-    
+from . import routes
