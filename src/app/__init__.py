e4ad1be829edc1df2cda79d8e4fc87809e7d3c76
Kyler Kramer <kyler@Kyler.Kylertk21>
2024-06-18 18:52:16 -0600

    item add and linkto orderform functional

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -2,9 +2,7 @@ from flask import Flask
 import os
 
 print("Work DIR: ", os.getcwd())
-
 template_dir = os.path.join(os.getcwd(), 'templates')
-print("Template folder: ", template_dir)
 
 app = Flask("Three Little Birds", template_folder=template_dir)
 app.secret_key = os.environ['SECRET_KEY']
--- src/app/routes.py
+++ src/app/routes.py
@@ -4,3 +4,27 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/marketplace')
+def marketplace():
+    catalog_items = [
+        {'image':'../static/photos/logo_placeholder.jpg', 'description':'Item 1 Description', 'link':'/orderform'}
+    ]
+
+    return render_template('marketplace.html', catalog_items=catalog_items)
+
+@app.route('/orderform')
+def orderform():
+    return render_template('orderform.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
