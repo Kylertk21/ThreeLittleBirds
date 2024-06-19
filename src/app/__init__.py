b31c894a9c7f1b7537d9f4c1849a76d4f9c68111
Kyler Kramer <kyler@Kyler.Kylertk21>
2024-06-18 22:34:51 -0600

    Init postgresql database & migrations

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://kyler:icedog@localhost/tlb'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 print("Work DIR: ", os.getcwd())
 template_dir = os.path.join(os.getcwd(), 'templates')
 
 app = Flask("Three Little Birds", template_folder=template_dir)
 app.secret_key = os.environ['SECRET_KEY']
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+
 
 from . import routes
