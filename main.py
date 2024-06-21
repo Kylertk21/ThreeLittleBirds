ae270a858839bd8ea7006e6eb060b987ec0a6c24
Kyler Kramer <kyler@Kyler.Kylertk21>
2024-06-21 17:02:30 -0600

    changes to main.py

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-from src.app import app
 import os
+import sys
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+app_dir = os.path.join(current_dir, 'src', 'app')
+os.chdir(app_dir)
+
+sys.path.insert(0, app_dir)
+
+from src.app import app
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
