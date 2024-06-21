081cc8cb7f7d4af65fe22f6fd0f194dd35725a02
Kyler Kramer <kyler@Kyler.Kylertk21>
2024-06-21 16:49:38 -0600

    Heroku setup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.app import app
+import os
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
 
 #MUST be in app dir
