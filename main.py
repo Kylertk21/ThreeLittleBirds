82ffe106a6f8598d50a80ac603c87910f42ca737
Kyler Kramer <kyler@Kyler.Kylertk21>
2024-06-26 14:05:59 -0600

    styling and structure changes

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ sys.path.insert(0, app_dir)
 from src.app import app
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', debug=True, port=os.environ.get('PORT', 5000))
 
 #MUST be in app dir
